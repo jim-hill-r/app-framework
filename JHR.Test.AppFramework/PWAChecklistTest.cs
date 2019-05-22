0161ac4e34a3d262b72014f85c53a76329e07f6e
Jim Hill <jim.hill.r@gmail.com>
2019-05-22 09:07:16 -0500

    Added test stubs for all checklist tests

---
+++ JHR.Test.AppFramework/PWAChecklistTest.cs
@@ -0,0 +1,59 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace JHR.Test.AppFramework
+{
+    // https://developers.google.com/web/progressive-web-apps/checklist
+
+    [TestClass]
+    public class PWAChecklistTest
+    {
+        [TestMethod]
+        public void ServedOverHTTPS()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void PagesAreResponsive()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void AllURLsLoadWhileOffline()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void MetadataProvided()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void FirstLoadFastOn3G()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void SiteWorksCrossBrowser()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TransitionsDontBlock()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void EachPageHasURL()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
