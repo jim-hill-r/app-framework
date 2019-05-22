c023560a4f858f0f8362222a83c637948b26d37f
Jim Hill <jim.hill.r@gmail.com>
2019-05-22 08:54:01 -0500

    Added tests

---
+++ JHR.Test.AppFramework/TestPWA.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace JHR.Test.AppFramework
+{
+    [TestClass]
+    public class TestPWA
+    {
+        [TestMethod]
+        public void HasManifestFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void HasServiceWorker()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
